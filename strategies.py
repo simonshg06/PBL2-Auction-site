546d693e2d76592105e28d838327bfb0bc8fe8fd
simonshg06 <simon.berger@esme.fr>
2026-04-08 12:54:24 +0200

    def __init_subclass

--- strategies.py
+++ strategies.py
@@ -4,3 +4,8 @@ import math
 class Strategy:
     name: str
     _registry: dict = {}
+
+    def __init_subclass__(cls):
+        if hasattr(cls, "name"):
+            Strategy._registry[cls.name.lower()] = cls
+
