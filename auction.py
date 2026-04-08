28bbfa22c38d5c0d6373dba9ad9b9c9bb5acc37d
simonshg06 <simon.berger@esme.fr>
2026-04-08 12:18:16 +0200

    place_bid function and parameter

--- auction.py
+++ auction.py
@@ -7,9 +7,11 @@ def bid_cost(price, base_cost=1.0, alpha=10.0):
 
 
 class AuctionRound:
-    
+
     def __init__(self, base_cost=1.0, alpha=10.0):
         self.base_cost, self.alpha = base_cost, alpha
         self.bst, self.bids, self.costs = BidBST(), [], {}
         self.winner, self.seller_revenue = None, 0.0
+
+    def place_bid(self, player, price):
 
