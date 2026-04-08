175ba82e92a0a000b5e790a5a6e4ee8b6cf392be
simonshg06 <simon.berger@esme.fr>
2026-04-08 11:49:21 +0200

    start readme

--- README.py
+++ README.py
@@ -1,2 +1,11 @@
-hello
+-#What does our projext do?
+-#How to install/run?
+-#What can i do?
 
+-#What does our APP do, features and requirements
+
+
+
+#Presentation:
+-#no code and +++ demo
+-# 7min
