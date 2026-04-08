962ca9798ea9ac5b16531428d3f1cfda53f28047
simonshg06 <simon.berger@esme.fr>
2026-04-08 12:54:44 +0200

    def bid

--- strategies.py
+++ strategies.py
@@ -9,3 +9,6 @@ class Strategy:
         if hasattr(cls, "name"):
             Strategy._registry[cls.name.lower()] = cls
 
+    def bid(self, round_number, history, base_cost, alpha, max_price=20):
+        raise NotImplementedError
+
