f8509b314583790f53d3b3ab433cd1234d5ffa89
simonshg06 <simon.berger@esme.fr>
2026-04-10 09:18:23 +0200

    class simulation

--- simulation.py
+++ simulation.py
@@ -7,3 +7,11 @@ class PlayerStats:
     wins: int = 0
     total_spent: float = 0.0
     total_profit: float = 0.0
+
+class Simulation:
+    def __init__(self, players, base_cost=1.0, alpha=10.0, max_price=20):
+        self.players = players
+        self.base_cost = base_cost
+        self.alpha = alpha
+        self.max_price = max_price
+        self._reset_stats()
