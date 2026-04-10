d4b4d17917d4a11200f7f60a24a55263a18d34de
simonshg06 <simon.berger@esme.fr>
2026-04-10 09:20:42 +0200

    def run_one_round

--- simulation.py
+++ simulation.py
@@ -28,3 +28,29 @@ class Simulation:
         for r in range(n_rounds):
             self._run_one_round(r, verbose and r % verbose_every == 0)
         self.rounds_played += n_rounds
+
+    def _run_one_round(self, round_num, verbose=False):
+        auction = AuctionRound(self.base_cost, self.alpha) # each player submits a bid based on their strategy
+        
+        for name, strategy in self.players:
+            price = strategy.bid(round_num, self.round_history,
+                                 self.base_cost, self.alpha, self.max_price)
+            auction.place_bid(name, price)
+        winner = auction.resolve()  # resolve lowest unique bid
+        self.round_history.append(auction.analysis())
+        self.seller_revenues.append(auction.seller_revenue) # update player stats: wins, spending, profit
+        
+        for name, _ in self.players:
+            ps = self.stats[name]
+            cost = auction.costs.get(name, 0.0)
+            ps.total_spent += cost
+            if winner and winner[1] == name:
+                ps.wins += 1
+                ps.total_profit += winner[0] - cost  # prize minus bid cost
+            else:
+                ps.total_profit -= cost  # lost bid is pure loss
+        
+        if winner is None:
+            self.no_winner_rounds += 1
+        if verbose:
+            auction.summary()
