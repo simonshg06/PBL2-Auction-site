fb9fe20d5f2f8f42dfd9f20c6e2d5984f90093bc
simonshg06 <simon.berger@esme.fr>
2026-04-10 09:36:31 +0200

    .

--- auction.py
+++ auction.py
@@ -10,7 +10,7 @@ class AuctionRound:
 
     def __init__(self, base_cost=1.0, alpha=10.0):
         self.base_cost, self.alpha = base_cost, alpha
-        self.bst, self.bids, self.costs = BidBST(), [], {}
+        self.bst, self.bids, self.costs = BidBst(), [], {}
         self.winner, self.seller_revenue = None, 0.0
 
     def place_bid(self, player, price):
