7939b0d238dde9cbb9b051f2d7eaf2af568f13eb
yousef12342241241 <yousef.abouturkia@esme.fr>
2026-04-11 14:40:42 +0200

    define seperator and press enter

--- Main.py
+++ Main.py
@@ -2,3 +2,11 @@ import random
 from auction import AuctionRound, bid_cost
 from BST import BidBst
 from strategies import DiceRoller, Cheapskate, Accountant, Historian, Hipster, Human
+
+def separator(title=""):
+    print(f"\n── {title} ──" if title else "\n" + "─" * 40)
+ 
+ 
+def press_enter():
+    input("\n  [Press Enter to continue]")
+
